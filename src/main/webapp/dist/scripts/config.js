73a6b13206d4a386ece1616b9fa338358d402321
xiangshaw <1542914328@qq.com>
2017-06-23 17:37:02 +0800

    1，提交

--- src/main/webapp/dist/scripts/config.js
+++ src/main/webapp/dist/scripts/config.js
@@ -3,9 +3,14 @@ require.config({
     shim:{
     	 'jquery':{
              exports: 'jquery'
+         },
+         'vue':{
+             exports: 'vue'
          }
     },
     paths: {
-    	'jquery' : "//cdn.bootcss.com/jquery/3.2.1/jquery.min"
+    	'jquery' : "//cdn.bootcss.com/jquery/3.2.1/jquery.min",
+    	'vue':"//cdn.bootcss.com/vue/2.3.4/vue.min.js"
+    		
     }
 });
